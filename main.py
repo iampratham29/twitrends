66e06f36b4b0abc4dd410cc075c5faab2e8307d5
iampratham29 <prathamesh.choudhari18@vit.edu>
2020-08-21 14:21:41 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+import time
+import pandas as pd
+cd='C:/Users/Prathmesh/Downloads/chromedriver_win32/chromedriver.exe'
+browser=webdriver.Chrome(cd)
+
+browser.get('https://twitter.com/explore/tabs/trending')
+time.sleep(15)
+sp=browser.find_elements_by_tag_name('span')
+fl=[]
+for i in sp:
+    a=i.get_attribute('textContent')
+    if(a.startswith('#')):
+        if a not in fl:
+            fl.append(a)
+urls=[]
+for i in fl:
+    i=i[1:]
+    url='htttps://twitter.com/search?q=%23'+i+'&src=trend_click'
+    urls.append(url)
+
+dict={'Hashtag':fl,'URL':urls}
+
+df=pd.DataFrame(dict)
+df.to_csv("trends.csv",index=False)
+print("done")
